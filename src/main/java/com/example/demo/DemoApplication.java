95fca38f557bc5ae83e60f35b3e7ddf97de74a86
LAB_C_REAL <labc@continental.edu.pe>
2023-11-03 18:37:52 -0500

    2do

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,13 @@
-package pe.edu.continental.demo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MiPrimerTrabajoConFormulariosConThymeleafApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MiPrimerTrabajoConFormulariosConThymeleafApplication.class, args);
-
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
